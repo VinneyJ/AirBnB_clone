7530bccab93777821978209290c845c0bee1467b
root <vincentjayden49@gmail.com>
2021-06-28 12:54:40 +0000

    corrected task 3 constructor

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, id=uuid.uuid4(), created_at=None, Updated_at=None):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
