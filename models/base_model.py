b1eb62b6b5985bfdbcd04cd2dd03c2f745d04d7b
root <vincentjayden49@gmail.com>
2021-06-28 12:43:39 +0000

    BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+    def to_dict(self):
+        dict = self.__dict__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        dict["__class__"] = self.__class__.__name__
+        return dict
